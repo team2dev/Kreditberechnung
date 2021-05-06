2b35eb9623fcca7a5c38d0416055e89def7f9f18
bgyb <bgyb@PC-34-4.BSZ-ET.lan.dd-schulen.de>
2021-05-06 09:21:42 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Kreditberechnung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Zahl1 = new System.Windows.Forms.Button();
+            this.Zahl2 = new System.Windows.Forms.Button();
+            this.Zahl3 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Zahl6 = new System.Windows.Forms.Button();
+            this.Zahl5 = new System.Windows.Forms.Button();
+            this.Zahl4 = new System.Windows.Forms.Button();
+            this.Zahl9 = new System.Windows.Forms.Button();
+            this.Zahl8 = new System.Windows.Forms.Button();
+            this.Zahl7 = new System.Windows.Forms.Button();
+            this.ZahlKomma = new System.Windows.Forms.Button();
+            this.Zahl0 = new System.Windows.Forms.Button();
+            this.ZahlVorzeichen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Zahl1
+            // 
+            this.Zahl1.Location = new System.Drawing.Point(280, 165);
+            this.Zahl1.Name = "Zahl1";
+            this.Zahl1.Size = new System.Drawing.Size(50, 50);
+            this.Zahl1.TabIndex = 1;
+            this.Zahl1.Text = "1";
+            this.Zahl1.UseVisualStyleBackColor = true;
+            // 
+            // Zahl2
+            // 
+            this.Zahl2.Location = new System.Drawing.Point(336, 165);
+            this.Zahl2.Name = "Zahl2";
+            this.Zahl2.Size = new System.Drawing.Size(50, 50);
+            this.Zahl2.TabIndex = 2;
+            this.Zahl2.Text = "2";
+            this.Zahl2.UseVisualStyleBackColor = true;
+            // 
+            // Zahl3
+            // 
+            this.Zahl3.Location = new System.Drawing.Point(392, 165);
+            this.Zahl3.Name = "Zahl3";
+            this.Zahl3.Size = new System.Drawing.Size(50, 50);
+            this.Zahl3.TabIndex = 3;
+            this.Zahl3.Text = "3";
+            this.Zahl3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(448, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 218);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Weiter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Zahl6
+            // 
+            this.Zahl6.Location = new System.Drawing.Point(392, 221);
+            this.Zahl6.Name = "Zahl6";
+            this.Zahl6.Size = new System.Drawing.Size(50, 50);
+            this.Zahl6.TabIndex = 7;
+            this.Zahl6.Text = "6";
+            this.Zahl6.UseVisualStyleBackColor = true;
+            // 
+            // Zahl5
+            // 
+            this.Zahl5.Location = new System.Drawing.Point(336, 221);
+            this.Zahl5.Name = "Zahl5";
+            this.Zahl5.Size = new System.Drawing.Size(50, 50);
+            this.Zahl5.TabIndex = 6;
+            this.Zahl5.Text = "5";
+            this.Zahl5.UseVisualStyleBackColor = true;
+            // 
+            // Zahl4
+            // 
+            this.Zahl4.Location = new System.Drawing.Point(280, 221);
+            this.Zahl4.Name = "Zahl4";
+            this.Zahl4.Size = new System.Drawing.Size(50, 50);
+            this.Zahl4.TabIndex = 5;
+            this.Zahl4.Text = "4";
+            this.Zahl4.UseVisualStyleBackColor = true;
+            // 
+            // Zahl9
+            // 
+            this.Zahl9.Location = new System.Drawing.Point(392, 277);
+            this.Zahl9.Name = "Zahl9";
+            this.Zahl9.Size = new System.Drawing.Size(50, 50);
+            this.Zahl9.TabIndex = 11;
+            this.Zahl9.Text = "9";
+            this.Zahl9.UseVisualStyleBackColor = true;
+            // 
+            // Zahl8
+            // 
+            this.Zahl8.Location = new System.Drawing.Point(336, 277);
+            this.Zahl8.Name = "Zahl8";
+            this.Zahl8.Size = new System.Drawing.Size(50, 50);
+            this.Zahl8.TabIndex = 10;
+            this.Zahl8.Text = "8";
+            this.Zahl8.UseVisualStyleBackColor = true;
+            // 
+            // Zahl7
+            // 
+            this.Zahl7.Location = new System.Drawing.Point(280, 277);
+            this.Zahl7.Name = "Zahl7";
+            this.Zahl7.Size = new System.Drawing.Size(50, 50);
+            this.Zahl7.TabIndex = 9;
+            this.Zahl7.Text = "7";
+            this.Zahl7.UseVisualStyleBackColor = true;
+            // 
+            // ZahlKomma
+            // 
+            this.ZahlKomma.Location = new System.Drawing.Point(392, 333);
+            this.ZahlKomma.Name = "ZahlKomma";
+            this.ZahlKomma.Size = new System.Drawing.Size(50, 50);
+            this.ZahlKomma.TabIndex = 15;
+            this.ZahlKomma.Text = ",";
+            this.ZahlKomma.UseVisualStyleBackColor = true;
+            // 
+            // Zahl0
+            // 
+            this.Zahl0.Location = new System.Drawing.Point(336, 333);
+            this.Zahl0.Name = "Zahl0";
+            this.Zahl0.Size = new System.Drawing.Size(50, 50);
+            this.Zahl0.TabIndex = 14;
+            this.Zahl0.Text = "0";
+            this.Zahl0.UseVisualStyleBackColor = true;
+            // 
+            // ZahlVorzeichen
+            // 
+            this.ZahlVorzeichen.Location = new System.Drawing.Point(280, 333);
+            this.ZahlVorzeichen.Name = "ZahlVorzeichen";
+            this.ZahlVorzeichen.Size = new System.Drawing.Size(50, 50);
+            this.ZahlVorzeichen.TabIndex = 13;
+            this.ZahlVorzeichen.Text = "+ / -";
+            this.ZahlVorzeichen.UseVisualStyleBackColor = true;
+            this.ZahlVorzeichen.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Kredit mit einmaliger Rückzahlung";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ZahlKomma);
+            this.Controls.Add(this.Zahl0);
+            this.Controls.Add(this.ZahlVorzeichen);
+            this.Controls.Add(this.Zahl9);
+            this.Controls.Add(this.Zahl8);
+            this.Controls.Add(this.Zahl7);
+            this.Controls.Add(this.Zahl6);
+            this.Controls.Add(this.Zahl5);
+            this.Controls.Add(this.Zahl4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Zahl3);
+            this.Controls.Add(this.Zahl2);
+            this.Controls.Add(this.Zahl1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Just4You - Kreditberechnung";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Zahl1;
+        private System.Windows.Forms.Button Zahl2;
+        private System.Windows.Forms.Button Zahl3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Zahl6;
+        private System.Windows.Forms.Button Zahl5;
+        private System.Windows.Forms.Button Zahl4;
+        private System.Windows.Forms.Button Zahl9;
+        private System.Windows.Forms.Button Zahl8;
+        private System.Windows.Forms.Button Zahl7;
+        private System.Windows.Forms.Button ZahlKomma;
+        private System.Windows.Forms.Button Zahl0;
+        private System.Windows.Forms.Button ZahlVorzeichen;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
